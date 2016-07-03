ce1cafc25f957317536751814c8381008f4fd3b5
suiyuchen <suiyc@xinboedu.com>
2016-07-03 18:35:10 +0800

    修改Test2Activity

--- android/src/com/insthub/ecmobile/Test2Activity.java
+++ android/src/com/insthub/ecmobile/Test2Activity.java
@@ -1,11 +1,9 @@
 package com.insthub.ecmobile;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
-public class Test2Activity extends ActionBarActivity {
+public class Test2Activity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -13,22 +11,4 @@ public class Test2Activity extends ActionBarActivity {
 		setContentView(R.layout.activity_test2);
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.test2, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
 }
