5def3b7500ff9f8e3e5d42eb7dd44115cdc7092e
woshiwuxu8 <993870408@qq.com>
2016-07-05 00:15:26 +0800

    创建一个新的Activity

--- android/src/com/insthub/ecmobile/MainActivity.java
+++ android/src/com/insthub/ecmobile/MainActivity.java
@@ -2,6 +2,8 @@ package com.insthub.ecmobile;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends Activity
 {
